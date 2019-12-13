77f465b167eb77834a54d22b2730cd11bdacb451
shpana <a.shaidullin@rdl-telecom.com>
2019-12-13 08:35:10 -0500

    ready

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -10,19 +10,37 @@ func InsertRecords(records []models.Record) error {
 	conn := connectDb("records app")
 	defer conn.Close()
 	for _, record := range records[1:] {
-		query := fmt.Sprintf(`INSERT INTO records(reg_number, name, url, phone, mail)
- VALUES ('%s', '%s', '%s', '%s', '%s');`,
-			record.RegNumber,
-			record.Name,
-			record.URL,
-			record.Phone,
-			record.Mail)
+		// check that record exists
+		queryCheck := fmt.Sprintf(`select exists(select reg_number from records where reg_number='%s');`,
+			record.RegNumber)
 
-		log.Println(query)
-
-		_, err := conn.Exec(query)
+		rows, err := conn.Query(queryCheck)
 		if err != nil {
 			return err
+		}
+		var exists bool
+		for rows.Next() {
+			err = rows.Scan(&exists)
+			if err != nil {
+				return err
+			}
+		}
+		if exists == false {
+			log.Println("creating new record: ", record)
+			queryInsert := fmt.Sprintf(`INSERT INTO records(reg_number, name, url, phone, mail)
+ VALUES ('%s', '%s', '%s', '%s', '%s');`,
+				record.RegNumber,
+				record.Name,
+				record.URL,
+				record.Phone,
+				record.Mail)
+
+			_, err = conn.Exec(queryInsert)
+			if err != nil {
+				return err
+			}
+		} else {
+			log.Printf("record with reg_number %s already exists", record.RegNumber)
 		}
 	}
 	return nil
